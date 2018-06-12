ae118cea60eab8524be2e0977f9f82913b3f7f7f
DHKIM <kdh30400@naver.com>
2018-06-12 16:26:08 +0900

    [김도형] 충돌체크 오류 수정 1. 노트와 도어의 depth 변경 2. 도어 콜리젼박스 변경 3. 노트 프리팹에 콜리젼박스 생성 3. 노트 소멸 위치의 y값 약간 밑으로 변경

--- Assets/Script/Unit/Door.cs
+++ Assets/Script/Unit/Door.cs
@@ -12,17 +12,36 @@ public class Door : MonoBehaviour
         // PC 에디터용
         if (Input.GetMouseButtonDown(0))
         {
-            Vector2 wp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Ray2D ray = new Ray2D(wp, Vector2.zero);
-            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
 
-            if (hit.collider != null)
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
             {
-                // 문을 선택 했다.
-                if(hit.transform.name == this.name)
+                if (hit.transform.name == this.name)
                     NoteManager.Instance.DeleteCheckNote(this);
                 Debug.Log("Complete" + hit.collider.name);
             }
+            else
+            {
+                Debug.Log("null");
+            }
+            /*
+                        Vector2 wp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                        Ray2D ray = new Ray2D(wp, Vector2.zero);
+                        RaycastHit hit = Physics.Raycast(ray.origin, ray.direction);
+
+                        if (hit.collider != null)
+                        {
+                            // 문을 선택 했다.
+                            if(hit.transform.name == this.name)
+                                NoteManager.Instance.DeleteCheckNote(this);
+                            Debug.Log("Complete" + hit.collider.name);
+                        }
+                        else
+                        {
+                            Debug.Log("null");
+                        }
+                        */
         }
 
         /*
