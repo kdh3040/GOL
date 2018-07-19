e0eb86edfc2179edf57c81c2e1f4e3457e3d28dd
kdh3040 <kdh30400@naver.com>
2018-07-19 14:11:47 +0900

    [김도형] 유니티 세팅중 #1

---
+++ Assets/Script/Manager/FirebaseManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirebaseManager : MonoBehaviour {
+
+    Firebase.Auth.FirebaseAuth auth;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void Awake()
+    {
+        auth = Firebase.Auth.FirebaseAuth.DefaultInstance;    
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    public void SignUp()
+    {
+        auth.SignInAnonymouslyAsync().ContinueWith(Task =>
+        {
+            if (Task.IsCanceled)
+            {
+                return;
+            }
+            if(Task.IsFaulted)
+            {
+                return;
+            }
+
+            Firebase.Auth.FirebaseUser newUser = Task.Result;
+
+            Debug.LogFormat("User signed in successfully: {0} ({1})",
+                    newUser.DisplayName, newUser.UserId);
+        });
+    }
+}
