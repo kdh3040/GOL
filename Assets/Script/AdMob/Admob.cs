bd1443920cb4e58e16eb15eccc5106b6aa27f683
kdh3040 <kdh30400@naver.com>
2018-08-09 16:56:44 +0900

    [김도형] 메인화면 상단 배너 적용 (Admob)

---
+++ Assets/Script/AdMob/Admob.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+
+public class Admob : MonoBehaviour {
+
+
+    private BannerView AdBanner;
+
+    // Use this for initialization
+    void Start () {
+        string adUnitID = "ca-app-pub-4020702622451243/9940740253";
+        AdBanner = new BannerView(adUnitID, AdSize.SmartBanner, AdPosition.Top);
+        AdRequest adRequest = new AdRequest.Builder().Build();
+        AdBanner.LoadAd(adRequest);
+        ShowAd();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ShowAd()
+    {
+        AdBanner.Show();
+    }
+}
