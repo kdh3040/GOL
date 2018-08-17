9015b2a0956520596c92b0560d675b1cbe5e3e2d
DESKTOP-EV9BEG1\Mangomilik <mangomilk114@gmail.com>
2018-08-17 20:23:49 +0900

    [배환웅] 노트 애니메이션 트리거 데이터 수정

--- Assets/Script/Data/NoteData.cs
+++ Assets/Script/Data/NoteData.cs
@@ -8,13 +8,11 @@ public class NoteData {
     public int id = 0;
     public int Score = 1;
     public string img;
-    public string ani_trigger;
 
     public NoteData(XmlNode node)
     {
         id = int.Parse(node.Attributes.GetNamedItem("id").Value);
         Score = int.Parse(node.Attributes.GetNamedItem("score").Value);
         img = node.Attributes.GetNamedItem("img").Value;
-        ani_trigger = node.Attributes.GetNamedItem("ani_trigger").Value;
     }
 }
