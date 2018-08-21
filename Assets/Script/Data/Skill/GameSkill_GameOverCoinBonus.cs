42e601cca172e2fb90c878484b3d7908fddb8c25
DESKTOP-EV9BEG1\Mangomilik <mangomilk114@gmail.com>
2018-08-21 16:31:50 +0900

    [배환웅] 코인 추가 스킬

---
+++ Assets/Script/Data/Skill/GameSkill_GameOverCoinBonus.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSkill_GameOverCoinBonus : GameSkill
+{
+    public GameSkill_GameOverCoinBonus(string name)
+        : base(name)
+    {
+
+    }
+
+    public override void SkillUpdate(float time)
+    {
+    }
+
+
+    public int ConvertCoin(int coin)
+    {
+        return coin + (int)(coin * mPercent);
+    }
+}
