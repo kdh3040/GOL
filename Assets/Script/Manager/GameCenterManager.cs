ce7c5a0fcd0a5a5cf35db5af64a57717a9bfedfc
DHKIM <kdh30400@naver.com>
2018-09-03 02:46:10 +0900

    [김도형] 게임센터 관련 기능 업로드

--- Assets/Script/Manager/GameCenterManager.cs
+++ Assets/Script/Manager/GameCenterManager.cs
@@ -129,6 +129,79 @@ public class GameCenterManager : MonoBehaviour {
         }
 
     }
-        
 
+    public void UnlockAchievement(int score)
+    {
+        if (score >= 1000)
+        {
+#if UNITY_ANDROID
+            PlayGamesPlatform.Instance.ReportProgress(GPGSIds.achievement_1000, 100f, null);
+#elif UNITY_IOS
+            Social.ReportProgress("Score_100", 100f, null);
+#endif
+        }
+    }
+
+    public void ShowAchievementUI()
+    {
+        // Sign In 이 되어있지 않은 상태라면
+        // Sign In 후 업적 UI 표시 요청할 것
+        if (Social.localUser.authenticated == false)
+        {
+            Social.localUser.Authenticate((bool success) =>
+            {
+                if (success)
+                {
+                    // Sign In 성공
+                    // 바로 업적 UI 표시 요청
+                    Social.ShowAchievementsUI();
+                    return;
+                }
+                else
+                {
+                    // Sign In 실패 처리
+                    return;
+                }
+            });
+        }
+
+        Social.ShowAchievementsUI();
+    }
+
+    public void ReportScore(int score)
+    {
+#if UNITY_ANDROID
+
+        PlayGamesPlatform.Instance.ReportScore(score, GPGSIds.leaderboard_score, (bool success) =>
+        {
+            if (success)
+            {
+                // Report 성공
+                // 그에 따른 처리
+            }
+            else
+            {
+                // Report 실패
+                // 그에 따른 처리
+            }
+        });
+
+#elif UNITY_IOS
+ 
+        Social.ReportScore(score, "Leaderboard_ID", (bool success) =>
+            {
+                if (success)
+                {
+                    // Report 성공
+                    // 그에 따른 처리
+                }
+                else
+                {
+                    // Report 실패
+                    // 그에 따른 처리
+                }
+            });
+        
+#endif
+    }
 }
