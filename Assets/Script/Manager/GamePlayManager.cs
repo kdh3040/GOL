71dc28b812f22bc37d48bf5b6a42482e861f044f
kdh3040 <kdh30400@naver.com>
2018-09-11 12:26:15 +0900

    [김도형] 설정 연동중

--- Assets/Script/Manager/GamePlayManager.cs
+++ Assets/Script/Manager/GamePlayManager.cs
@@ -239,7 +239,10 @@ public class GamePlayManager : MonoBehaviour
     {
         mGameUIPage.GameOver();
         IsGamePause = true;
-        SettingManager.Instance.DoVibe();
+        if(PlayerData.Instance.GetVibrationSetting())
+        {
+            Handheld.Vibrate();
+        }        
         AdManager.Instance.ShowInterstitialAd();
         StartCoroutine(Co_GameOver(note));
     }
@@ -369,14 +372,21 @@ public class GamePlayManager : MonoBehaviour
 
     public void PlayGetItemSound()
     {
-        mAudio.clip = mClip[0];
-        mAudio.Play();
+        if (PlayerData.Instance.GetSoundSetting())
+        {
+            mAudio.clip = mClip[0];
+            mAudio.Play();
+        }
+        
     }
 
     public void PlayUseItemSound()
     {
-        mAudio.clip = mClip[2];
-        mAudio.Play();
+        if (PlayerData.Instance.GetSoundSetting())
+        {
+            mAudio.clip = mClip[2];
+            mAudio.Play();
+        }
     }
 
     public void EndSkill(GameSkill skill)
