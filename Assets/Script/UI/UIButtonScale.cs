6d715a9f5393075a03821ea6f13ad424f250dab6
DESKTOP-EV9BEG1\Mangomilik <mangomilk114@gmail.com>
2018-09-13 19:59:44 +0900

    [배환웅] 버튼 스케일 추가

---
+++ Assets/Script/UI/UIButtonScale.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UIButtonScale : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    Vector3 OriginalScale;
+    void Start()
+    {
+        OriginalScale = gameObject.transform.localScale;
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        this.gameObject.transform.localScale = new Vector3(OriginalScale.x * 0.8f, OriginalScale.y * 0.8f, OriginalScale.z * 0.8f);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        this.gameObject.transform.localScale = OriginalScale;
+    }
+
+}
+
