ae3794da5b94c5d0efec6f8b155b381f5e6466e8
kdh3040 <kdh30400@naver.com>
2018-09-14 13:21:45 +0900

    [김도형] 1. 로딩화면 이미지 적용 2. 문닫는 이펙트 위치 조절

---
+++ Assets/Script/Manager/SoundManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+    public static SoundManager _instance = null;
+    public static SoundManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<SoundManager>() as SoundManager;
+            }
+            return _instance;
+        }
+
+    }
+
+    private int soundFX;
+
+    // Use this for initialization
+    void Start () {
+        DontDestroyOnLoad(this);
+        soundFX = AudioCenter.loadSound("Sound/click");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PlayFXSound()
+    {
+        if (PlayerData.Instance.GetSoundSetting() == true)
+        {
+
+            AudioCenter.playSound(soundFX);
+            //mFX.Play();
+        }
+    }
+}
