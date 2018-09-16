92b9132d585ba020996e785a38902d30d453c451
DESKTOP-EV9BEG1\Mangomilik <mangomilk114@gmail.com>
2018-09-16 23:25:24 +0900

    [배환웅] 노트 속도 조절 가능한 테스트 기능 추가

--- Assets/Script/UI/Popup/PopupGameSetting.cs
+++ Assets/Script/UI/Popup/PopupGameSetting.cs
@@ -18,6 +18,12 @@ public class PopupGameSetting : PopupUI
     public GameObject VibrationSettingCheck;
     public GameObject AlarmSettingCheck;
 
+    public List<InputField> Test = new List<InputField>();
+
+    public static float speed_1 = 0f; // 기본
+    public static float speed_2 = 0f; // 증가 시간
+    public static float speed_3 = 0f; // 증가폭
+
     public void Awake()
     {
         SoundSettingButton.onClick.AddListener(OnClickSoundSetting);
@@ -32,6 +38,27 @@ public class PopupGameSetting : PopupUI
         SoundSettingCheck.SetActive(PlayerData.Instance.SoundSetting);
         VibrationSettingCheck.SetActive(PlayerData.Instance.VibrationSetting);
         AlarmSettingCheck.SetActive(PlayerData.Instance.AlarmSetting);
+
+        if(speed_1 == 0)
+            Test[0].text = string.Format("{0:F2}", ConfigData.Instance.DEFAULT_NOTE_SPEED + speed_1);
+        else
+            Test[0].text = string.Format("{0:F2}", speed_1);
+        if(speed_2 == 0)
+            Test[1].text = string.Format("{0:F2}", ConfigData.Instance.NOTE_SPEED_UP_INTERVAL + speed_2);
+        else
+            Test[1].text = string.Format("{0:F2}", speed_2);
+        if(speed_3 == 0)
+            Test[2].text = string.Format("{0:F2}", ConfigData.Instance.NOTE_SPEED_UP + speed_3);
+        else
+            Test[2].text = string.Format("{0:F2}", speed_3);
+    }
+
+    public override void DismissPopup()
+    {
+        base.DismissPopup();
+        speed_1 = float.Parse(Test[0].text);
+        speed_2 = float.Parse(Test[1].text);
+        speed_3 = float.Parse(Test[2].text);
     }
 
     public void OnClickSoundSetting()
