3c75590143cc3c8aee5f1bbde3bb02ef926042ec
DESKTOP-EV9BEG1\Mangomilik <mangomilk114@gmail.com>
2018-09-21 13:48:55 +0900

    [배환웅] 첫 실행시 코인 추가

--- Assets/Script/Data/PlayerData.cs
+++ Assets/Script/Data/PlayerData.cs
@@ -167,7 +167,7 @@ public class PlayerData
             SkinSlotLevel.Add(CommonData.SKIN_TYPE.CHAR, 1);
             SkinSlotLevel.Add(CommonData.SKIN_TYPE.DOOR, 1);
             //MyCoin = 1000;
-            MyCoin = 100;
+            MyCoin = 2000000;
             MyDDong = 30;// CommonData.MAX_DDONG_COUNT;
             NextDDongRefilTime = DateTime.MinValue;
             SoundSetting = true;
